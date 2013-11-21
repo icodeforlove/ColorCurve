ace5603a9f448307c5b7cdf4372853c91a23a3e7
Chad Scira <chadvscira@gmail.com>
2013-11-20 18:11:56 -0800

    added a display for the RGB curve point settings

--- example/script.js
+++ example/script.js
@@ -3,64 +3,62 @@ var Filters = Filters || {};
 Filters.pixels = [];
 var CC1,CC2,CC3;
 
-Filters.Init = function(img) 
+Filters.Init = function(img)
 {
-  var c = document.getElementById('CvOriginal');
-  c.style.display = 'none';  
-  var ctx = c.getContext('2d');
-  ctx.drawImage(img,0,0);
-  
-  var imageData = ctx.getImageData(0,0, c.width,c.height);
-  Filters.numPixels = imageData.width * imageData.height;
-  Filters.pixels = imageData.data; 
+	var c = document.getElementById('CvOriginal');
+	c.style.display = 'none';
+	var ctx = c.getContext('2d');
+	ctx.drawImage(img,0,0);
 
-  var c = document.getElementById('CvModified');
-  var ctx = c.getContext('2d');
-  ctx.drawImage(img,0,0);
+	var imageData = ctx.getImageData(0,0, c.width,c.height);
+	Filters.numPixels = imageData.width * imageData.height;
+	Filters.pixels = imageData.data;
 
-  this.canvas = document.getElementById('CvModified');
-  this.ctx = Filters.canvas.getContext('2d');
-  
-  
+	var c = document.getElementById('CvModified');
+	var ctx = c.getContext('2d');
+	ctx.drawImage(img,0,0);
+
+	this.canvas = document.getElementById('CvModified');
+	this.ctx = Filters.canvas.getContext('2d');
 }
-
 
 Filters.getPixels = function(filter) {
 
-  var imageData = this.ctx.getImageData(0,0, this.canvas.height,this.canvas.width);
-  var pix = imageData.data; 
+	var imageData = this.ctx.getImageData(0,0, this.canvas.height,this.canvas.width);
+	var pix = imageData.data;
 
-  filter(Filters.pixels,pix);
-  Filters.ctx.putImageData(imageData, 0, 0);
+	filter(Filters.pixels,pix);
+	Filters.ctx.putImageData(imageData, 0, 0);
 
 }
 
 Filters.ApplyFilter = function(idatasource, idataout) {
-  
+	if (CC1 == undefined || CC2 == undefined || CC3 == undefined) return;
 
-  if (CC1 == undefined || CC2 == undefined || CC3 == undefined) return;  
-  for (var i = 0; i < Filters.numPixels; i++) { 
-      idataout[i*4] = CC1.rgb[idatasource[i*4]];  
-      idataout[i*4+1] = CC2.rgb[idatasource[i*4+1]];  
-      idataout[i*4+2] = CC3.rgb[idatasource[i*4+2]];
-    }
+	document.getElementById('settings').innerHTML = JSON.stringify({
+		r: Array.prototype.concat.apply([],CC1.points.map(function (point) {return [parseFloat(point.x.toFixed(4)), parseFloat(point.y.toFixed(4))]})),
+		g: Array.prototype.concat.apply([],CC2.points.map(function (point) {return [parseFloat(point.x.toFixed(4)), parseFloat(point.y.toFixed(4))]})),
+		b: Array.prototype.concat.apply([],CC3.points.map(function (point) {return [parseFloat(point.x.toFixed(4)), parseFloat(point.y.toFixed(4))]}))
+	}).replace(/"/g, '');
+
+	for (var i = 0; i < Filters.numPixels; i++) {
+			idataout[i*4] = CC1.rgb[idatasource[i*4]];
+			idataout[i*4+1] = CC2.rgb[idatasource[i*4+1]];
+			idataout[i*4+2] = CC3.rgb[idatasource[i*4+2]];
+		}
 }
-
 
 function start() {
-  var imageObj = new Image();
-  imageObj.src = 'image.jpg';
-  imageObj.onload = function() {    
-    Filters.Init(imageObj);
-    CC1 = new ColorCurve('selectorR', function(){ Filters.getPixels(Filters.ApplyFilter);     });
-    CC2 = new ColorCurve('selectorG', function(){ Filters.getPixels(Filters.ApplyFilter);     });
-    CC3 = new ColorCurve('selectorB', function(){ Filters.getPixels(Filters.ApplyFilter);     });
-  };
+	var imageObj = new Image();
+	imageObj.src = 'image.jpg';
+	imageObj.onload = function() {
+		Filters.Init(imageObj);
+		CC1 = new ColorCurve('selectorR', function(){ Filters.getPixels(Filters.ApplyFilter);     });
+		CC2 = new ColorCurve('selectorG', function(){ Filters.getPixels(Filters.ApplyFilter);     });
+		CC3 = new ColorCurve('selectorB', function(){ Filters.getPixels(Filters.ApplyFilter);     });
+
+		CC1.onchange();
+	};
 
 
- 
 }
-
-
-
-
--- src/Colorcurve.js
+++ src/Colorcurve.js
@@ -26,7 +26,7 @@ function ColorCurve(canvas, callback)
 	this.c.onmousedown = function(ev) { me.MouseDown(ev); }
 	this.c.onmouseup = function(ev) { me.MouseUp(ev);  me.Draw(); }
 	this.c.onmouseout = function(ev) { me.MouseUp(ev);  me.Draw();}
-	this.c.onmousemove = function(ev) { 
+	this.c.onmousemove = function(ev) {
 		me.MouseMove(ev);
 		if (me.redraw == 1) {
 			me.Draw();
@@ -41,7 +41,7 @@ function ColorCurve(canvas, callback)
 // Update the RGB array to fit the new curve values. Transform curve points to 0..255 values
 ColorCurve.prototype.UpdateValues = function()
 {
-	this.rgb.splice(0, this.rgb.length);	
+	this.rgb.splice(0, this.rgb.length);
 	for(var i=0;i<256;i++) this.rgb.push(Math.round(this.GetY(i/255.0)*255));
 
 	this.onchange();
@@ -56,39 +56,39 @@ ColorCurve.prototype.IsEqual = function(p1,p2)
 }
 
 // Draw the curve
-ColorCurve.prototype.Draw = function() 
+ColorCurve.prototype.Draw = function()
 {
 	this.values.splice(0, this.values.length);
 	this.ctx.clearRect(0, 0, this.width, this.height);
 	this.DrawGrid();
-	
+
 	for(i=0;i<this.points.length-1;i++)
 	{
-		if (i<1) { 
+		if (i<1) {
 			p1 = this.points[0];
-		} else { 
+		} else {
 			p1 = this.points[i-1];
-		}	 
+		}
 		if (i+2 > this.points.length-1) {
 			p4 = this.points[i+1];
-		} else { 
+		} else {
 			p4 = this.points[i+2];
-		} 
+		}
 		this.Quadratic(p1,this.points[i],this.points[i+1],p4);
 	}
 	this.DrawPoints();
-	
+
 }
 
 // The background grid
-ColorCurve.prototype.DrawGrid = function() 
+ColorCurve.prototype.DrawGrid = function()
 {
-	var space = this.width/4.0;	
+	var space = this.width/4.0;
 
 	this.ctx.beginPath();
 	this.ctx.lineWidth = 1;
 	this.ctx.strokeStyle = '#aaaaaa';
-	
+
 	for(i=0;i<this.height-space;i+=space)
 	{
 		this.ctx.moveTo(0, i+space); this.ctx.lineTo(this.height, i+space);
@@ -103,7 +103,7 @@ ColorCurve.prototype.DrawGrid = function()
 // Main function. Calculate curve coeficients and draw the curve
 ColorCurve.prototype.Quadratic = function(p1,p2,p3,p4)
 {
-	this.ctx.strokeStyle = '#ffffff'; 
+	this.ctx.strokeStyle = '#ffffff';
   	this.ctx.lineWidth = 1.5;
   	var slope = 0;
 
@@ -116,7 +116,7 @@ ColorCurve.prototype.Quadratic = function(p1,p2,p3,p4)
 	dx = x3 - x0;
 	dy = y3 - y0;
 
-	x1 = ((2.0*x0)/3.0) + (x3/3.0)    
+	x1 = ((2.0*x0)/3.0) + (x3/3.0)
 	x2 = (x0/3.0) + ((2.0*x3)/3.0);
 
 
@@ -124,16 +124,16 @@ ColorCurve.prototype.Quadratic = function(p1,p2,p3,p4)
 	{
       y1 = y0 + (dy / 3.0);
       y2 = y0 + ((dy * 2.0) / 3.0);
- 	} 
+ 	}
 	if (this.IsEqual(p1,p2) && !this.IsEqual(p3,p4) )
 	{
 
 		slope = ((p4.y) - y0) / (p4.x - x0);
 		y2 = y3 - ((slope * dx) / 3.0);
 		y1 = y0 + ((y2 - y0) / 2.0);
- 
+
 	}
-	if (!this.IsEqual(p1,p2) && this.IsEqual(p3,p4) ) 
+	if (!this.IsEqual(p1,p2) && this.IsEqual(p3,p4) )
 	{
       slope = (y3 - (p1.y)) / (x3 - p1.x);
 
@@ -148,13 +148,13 @@ ColorCurve.prototype.Quadratic = function(p1,p2,p3,p4)
 		y2 = y3 - ((slope * dx) / 3.0);
  	}
 
-	this.ctx.beginPath(); 	
-	this.ctx.moveTo(x0*this.width, this.height-(y0*this.height)); 
+	this.ctx.beginPath();
+	this.ctx.moveTo(x0*this.width, this.height-(y0*this.height));
 
-	step =(x3-x0)/20.0;	
+	step =(x3-x0)/20.0;
 	tx = x0;
 	for(var i=0.0;i<=1.05;i+=0.05)
-		{	
+		{
 		ty =     (y0 * Math.pow((1-i),3)) +
 	  		(3 * y1 * Math.pow((1-i),2) * i)     +
 	  		(3 * y2 * (1-i) * i     * i)     +
@@ -171,20 +171,20 @@ ColorCurve.prototype.Quadratic = function(p1,p2,p3,p4)
 
 	this.ctx.moveTo(this.points[3].x*this.width, this.height-(this.points[3].y*this.height));
 	this.ctx.lineTo(this.width,this.height-(this.points[3].y*this.height));
-	
+
 	this.ctx.stroke();
 
 
 	// Uncomment this for view the control points
 	/*
-	this.ctx.fillStyle = '#00ff00'; 
-	this.ctx.beginPath();	
+	this.ctx.fillStyle = '#00ff00';
+	this.ctx.beginPath();
 
 	this.ctx.moveTo(x1,y1);
 	this.ctx.arc(x1,y1, 3, 0 , 2 * Math.PI, false);
 	this.ctx.moveTo(x2,y2);
 	this.ctx.arc(x2,y2, 3, 0 , 2 * Math.PI, false);
-	
+
 	this.ctx.fill();
 	*/
 	return true;
@@ -192,29 +192,29 @@ ColorCurve.prototype.Quadratic = function(p1,p2,p3,p4)
 }
 
 // Draw the control points
-ColorCurve.prototype.DrawPoints = function() 
+ColorCurve.prototype.DrawPoints = function()
 {
-	
-	this.ctx.fillStyle = '#ff0000'; 
-	this.ctx.beginPath();	
+
+	this.ctx.fillStyle = '#ff0000';
+	this.ctx.beginPath();
 
  	for(i=0;i<this.points.length;i++)
- 	{ 
+ 	{
 		this.ctx.moveTo(this.points[i].x*this.width,this.height-(this.points[i].y*this.height));
 		this.ctx.arc(this.points[i].x*this.width,this.height-(this.points[i].y*this.height), 3, 0 , 2 * Math.PI, false);
-		
+
  	}
 	this.ctx.fill();
- 	
+
 }
 
 
-ColorCurve.prototype.MouseDown = function(event) 
+ColorCurve.prototype.MouseDown = function(event)
 {
 	if(!event) var event = window.event;
     var x = (event.pageX-this.c.offsetLeft)/this.width,
         y = (event.pageY-this.c.offsetTop)/this.height;
-	
+
 	dis = 10000;
 	punto = -1;
 
@@ -225,21 +225,21 @@ ColorCurve.prototype.MouseDown = function(event)
 
 		tdis = x1*x1+y1*y1;
 		tdis = Math.sqrt(tdis);
-		
-		if (tdis < dis) { 
+
+		if (tdis < dis) {
 			dis = tdis;
 			punto = i;
 		}
-		
-	}	
-	this.currentpoint = (dis < 8.0) ? punto : this.currentpoint;	
+
+	}
+	this.currentpoint = (dis < 8.0) ? punto : this.currentpoint;
 
 }
 
 
 ColorCurve.prototype.MouseUp = function(event) {
-   
-	if (this.currentpoint != -1) { 
+
+	if (this.currentpoint != -1) {
 		this.UpdateValues();
 	}
 	this.currentpoint = -1;
@@ -247,20 +247,20 @@ ColorCurve.prototype.MouseUp = function(event) {
 }
 
 ColorCurve.prototype.MouseMove = function(event) {
-   
+
 	if (this.currentpoint == -1) return;
 
 	if (this.currentpoint > 0) prevx = this.points[this.currentpoint-1].x; else prevx = 0;
-	if (this.currentpoint==this.points.length-1) nextx = 1.0; else nextx = this.points[this.currentpoint+1].x; 
-	
+	if (this.currentpoint==this.points.length-1) nextx = 1.0; else nextx = this.points[this.currentpoint+1].x;
+
 	x = (event.pageX-this.c.offsetLeft)/this.width;
     y = 1.0-((event.pageY-this.c.offsetTop)/this.height);
 
      if(x > prevx && x < nextx) {
 		this.points[this.currentpoint].x = x;
 		this.points[this.currentpoint].y = y;
-	
-		this.redraw = 1;		
+
+		this.redraw = 1;
 	}
 
 
@@ -270,7 +270,7 @@ ColorCurve.prototype.MouseMove = function(event) {
 // Return the normalized Y value for the specified X value. X should be passed normnalized too
 ColorCurve.prototype.GetY = function(xpos)
 {
-	
+
 	if (xpos < this.values[0].x) xpos = this.values[0].x;
 	if (xpos > this.values[this.values.length-1].x) xpos = this.values[this.values.length-1].x;
 
